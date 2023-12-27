6f24b11faad1463c6bf16ae5d0970cbc2333f07c
pelsapupel <79982439+pelsapupel@users.noreply.github.com>
2023-12-27 13:06:35 +0700

    Create query_part_today_4.sh

---
+++ prod/query_part_today_4.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+. ~/etc/db
+. ~/etc/job
+
+export SQL=query_part_today_4.sql
+export LOG=~/log/$(basename ${SQL} .sql).log
+touch ${LOG}
+
+cd ${WORKDIR}
+echo $(date) $SQL starting >> ${LOG}
+
+flock -xn "${SQL}" -c "mysql -A -h 10.1.3.23 -u engineer -p'XXXXXX' staging < ~/query_part_today_4.sql >> ${LOG} 2>&1"
+
+echo $(date) $SQL end >> ${LOG}
+
+#!/bin/bash
+. ~/etc/db
+. ~/etc/job
+
+export SQL=move_query_part_today_4.sql
+export LOG=~/log/$(basename ${SQL} .sql).log
+touch ${LOG}
+
+cd ${WORKDIR}
+echo $(date) $SQL starting >> ${LOG}
+
+flock -xn "${SQL}" -c "mysql -A -h 10.1.3.25 -u engineer -p'XXXXXX' staging < ~/move_query_part_today_4.sql >> ${LOG} 2>&1"
+
+echo $(date) $SQL end >> ${LOG}
